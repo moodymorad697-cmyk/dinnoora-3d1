c0ef2183ae13c099c4c97f140c9fae93940d08e7
moodymorad697-cmyk <moodymorad697@gmail.com>
2026-04-27 18:37:04 +0800

    Refactor Breadcrumb components and add separator
    fix breadcrumb ref type error (remove Slot)

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -1,12 +1,46 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface BreadcrumbLinkProps
-  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
+export function Breadcrumb({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <nav
+      aria-label="breadcrumb"
+      className={cn(className)}
+      {...props}
+    />
+  )
+}
+
+export function BreadcrumbList({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLOListElement>) {
+  return (
+    <ol
+      className={cn("flex items-center space-x-2 text-sm", className)}
+      {...props}
+    />
+  )
+}
+
+export function BreadcrumbItem({
+  className,
+  ...props
+}: React.LiHTMLAttributes<HTMLLIElement>) {
+  return (
+    <li
+      className={cn("inline-flex items-center", className)}
+      {...props}
+    />
+  )
+}
 
 export const BreadcrumbLink = React.forwardRef<
   HTMLAnchorElement,
-  BreadcrumbLinkProps
+  React.AnchorHTMLAttributes<HTMLAnchorElement>
 >(({ className, ...props }, ref) => {
   return (
     <a
@@ -18,3 +52,17 @@ export const BreadcrumbLink = React.forwardRef<
 })
 
 BreadcrumbLink.displayName = "BreadcrumbLink"
+
+export function BreadcrumbSeparator({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLSpanElement>) {
+  return (
+    <span
+      className={cn("mx-1", className)}
+      {...props}
+    >
+      /
+    </span>
+  )
+}
